075067962a1f8f220331b1b622e746197f30c621
Emati Mitame <GitCommits@mita.me>
2016-04-06 12:28:32 +0100

    add simple-array-sum

---
+++ algorithms/warmup/simple-array-sum.c
@@ -0,0 +1,26 @@
+// https://www.hackerrank.com/challenges/simple-array-sum
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char const *argv[]) {
+  int num_items;
+  scanf("%d\n", &num_items);
+
+  char num_string[4096];
+  fgets(num_string, 4095, stdin);
+
+  char * num_str;
+
+  int sum = 0;
+  char * endptr;
+
+  num_str = strtok(num_string, " ");
+  for (int i = 0; i < num_items; i++, num_str = strtok(NULL, " "))
+    sum += strtol(num_str, &endptr, 10);
+
+  printf("%d\n", sum);
+
+  return 0;
+}
