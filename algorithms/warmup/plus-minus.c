d40ae3b4942cdbdff6c5439be097647bbdd800d7
Emati Mitame <GitCommits@mita.me>
2016-04-06 12:58:04 +0100

    Add plus-minus

---
+++ algorithms/warmup/plus-minus.c
@@ -0,0 +1,27 @@
+// https://www.hackerrank.com/challenges/plus-minus
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  int n;
+  scanf("%d\n", &n);
+
+  int num;
+  int n_count = 0;
+  int p_count = 0;
+  int z_count = 0;
+  for(int i = 0; i < n; i++){
+    scanf("%d", &num);
+    if (num > 0)
+      p_count++;
+    else if (num < 0)
+      n_count++;
+    else
+      z_count++;
+  }
+  printf("%6f\n", (float)p_count / n);
+  printf("%6f\n", (float)n_count / n);
+  printf("%6f\n", (float)z_count / n);
+
+  return 0;
+}
