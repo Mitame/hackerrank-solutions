c67b2d7391abaac7a07841049cfb4c17bd05137d
Emati Mitame <GitCommits@mita.me>
2016-04-07 11:02:03 +0100

    Add use c99 directive to cmake

--- algorithms/implementation/bear-and-workbook.c
+++ algorithms/implementation/bear-and-workbook.c
@@ -1,6 +1,11 @@
 // https://www.hackerrank.com/challenges/bear-and-workbook
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char const *argv[]) {
-  /* code */
+  int n, k;
+  scanf("%d %d", &n, &k);
+
   return 0;
 }
